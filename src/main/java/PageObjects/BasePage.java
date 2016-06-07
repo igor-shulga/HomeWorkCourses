4e3bc9c13e257e219aeefa96d49ee2a649c34c28
Igor Shulga <igorshulga@yahoo.com>
2016-06-07 20:20:37 +0300

    upd

--- src/main/java/PageObjects/BasePage.java
+++ src/main/java/PageObjects/BasePage.java
@@ -7,9 +7,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.swing.*;
+import java.util.List;
 
 
 public class BasePage {
@@ -27,22 +29,24 @@ public class BasePage {
     }
 
     public void openPage(){
-        driver.navigate().to("http://semantic-ui.com/elements/input.html");
-        Actions reloadPage = new Actions(driver);
-        reloadPage.keyDown(Keys.CONTROL).sendKeys(Keys.F5).perform();
+        driver.navigate().to("http://semantic-ui.com/modules/dropdown");
 
-        WebDriverWait wait = new WebDriverWait(driver,10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(input));
+      //  WebDriverWait wait = new WebDriverWait(driver,10);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='example']/div[4]/div[1]/div[2]/div[2]/div[1]/div[4]/div")));
 
+        WebElement dropDownListBox = driver.findElement(By.xpath(".//*[@id='example']/div[4]/div[1]/div[2]/div[2]/div[1]/div[4]/div")).findElement(By.tagName("select"));
+        Select clickThis = new Select(dropDownListBox);
+       // clickThis.getOptions().;
+        System.out.println( clickThis.getOptions().size());
 
-
+        }
 
        // System.out.println(element.getAttribute("value"));
     }
 
 
     /* get value from form
-
+    driver.navigate().to("http://semantic-ui.com/elements/input.html");
      private By input = By.xpath(".//*[@id='example']/div[4]/div[1]/div[2]/div[2]/div[22]/div/input");
 
     driver.navigate().to("http://semantic-ui.com/elements/input.html");
@@ -53,6 +57,13 @@ public class BasePage {
         System.out.println(element.getAttribute("value"));
      */
 
+    /* reload page and wait till element will be visible
+     driver.navigate().to("http://semantic-ui.com/elements/input.html");
+        Actions reloadPage = new Actions(driver);
+        reloadPage.keyDown(Keys.CONTROL).sendKeys(Keys.F5).perform();
+
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(input));
+     */
 
 
-}
